90649d3b69b53f910588d55162b1e1dc29498b6b
Sampath <bogasampathkumar@gmail.com>
2021-04-29 18:25:59 +0530

    commit4

--- src/components/Repositories/Repositories.js
+++ src/components/Repositories/Repositories.js
@@ -1,57 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import axios from 'axios';
 import './Repositories.css';
 import moment from 'moment';
 
 
-class Repositories extends Component {
-    state = { 
-        repositories : [],
-        showRep : true,
-        text : '',
-        lang : '',
-        noOfRepos: '',
-        noOfStars: '',
-        noOfFollowers: '',
-        noOfFollowing: ''
-     }
+const Repositories =props =>{
     
-    componentDidMount(){
-        axios.get('https://api.github.com/users/supreetsingh247/repos').then(resp =>{
-            if(resp){
-            console.log(resp);
-            this.setState({repositories : resp.data})
-            }
-        })
-
-        axios.get('https://api.github.com/users/supreetsingh247').then(resp =>{
-            if(resp){
-            //console.log(resp);
-            this.setState({noOfRepos : resp.data.public_repos, 
-                        noOfFollowers: resp.data.followers, 
-                        noOfFollowing: resp.data.following})
-            
-            }
-        })
-    }
-
-    langHandleChange(event){
-        this.setState({lang: event.target.value});
-    }
-
-    render() { 
-
+    const[text, setText] = useState('');
+    const[lang, setLang] = useState('');
+  
         
-        console.log('-------------'+this.state.lang);
-
-       let repositorie = '';
-        if(this.state.showRep){
-            if(this.state.repositories){
-                let x=0;
-            repositorie = this.state.repositories.map(rep =>{
+       let repositorie = 'Something went wrong..!';
+        
+       if(props.repositories){
+            
+            repositorie = props.repositories.map(rep =>{
                 
                 let lastUpdated = moment(rep.updated_at).fromNow();
-                x++;
+                
                 return(
                   <div key={rep.id} class="repo-items">
                   <h2>{rep.name}</h2>
@@ -61,14 +27,14 @@ class Repositories extends Component {
                 );
                 
             })
-            }
+        }
 
-            if(this.state.text){
-                let x=0;
-            repositorie = this.state.repositories.filter(rep => rep.name.toLowerCase().includes(this.state.text)).map(rep =>{
+        if({text}){
+                
+            repositorie = props.repositories.filter(rep => rep.name.toLowerCase().includes({text})).map(rep =>{
                 
                 let lastUpdated = moment(rep.updated_at).fromNow();
-                x++;
+                
                 return(
                   <div key={rep.id} class="repo-items">
                   <h2>{rep.name}</h2>
@@ -78,14 +44,14 @@ class Repositories extends Component {
                 );
                 
             })
-            }
+        }
 
-            if(this.state.lang){
-                let x=0;
-            repositorie = this.state.repositories.filter(rep => rep.language.toLowerCase().includes(this.state.lang)).map(rep =>{
+        if({lang}){
+                
+            repositorie = props.repositories.filter(rep => rep.language.toLowerCase().includes({lang})).map(rep =>{
                 
                 let lastUpdated = moment(rep.updated_at).fromNow();
-                x++;
+                
                 return(
                   <div key={rep.id} class="repo-items">
                   <h2>{rep.name}</h2>
@@ -95,8 +61,6 @@ class Repositories extends Component {
                 );
                 
             })
-            }
-
         }
 
         return (
@@ -108,7 +72,7 @@ class Repositories extends Component {
                   </li>
                   <li>
                       <a href="#news">Repositories
-                          <span class="count">{this.state.noOfRepos}</span></a>
+                          <span class="count">{props.repositories.}</span></a>
                   </li>
                   <li>
                       <a href="#contact">Stars
@@ -125,7 +89,7 @@ class Repositories extends Component {
               </ul>
             </div>
             <div class="search-bar">
-               <input type="text" onChange={(e) => this.setState({ text: e.target.value })} 
+               <input type="text" onChange={(e) => setText(e)} 
                   placeholder="Search repositories..." name="search"></input>
 
                <select name="type">
@@ -157,72 +121,6 @@ class Repositories extends Component {
           </div>
 
         );
-    }
 }
 export default Repositories;
 
-{/* 
-<div className="rep-body">
-<div className="rep-row-1">
-<div className="rep-nav">
-   <ul>
-     <li>Overview</li>
-     <li 
-       style={{ backgroundColor: "#E0D9D8", borderRadius: "4px" }}
-     >
-       Repositories
-     </li>
-     <li>Stars</li>
-   </ul>
-</div>
-/div>
-
-<div className="rep-row-2">
-<div className="filter-input">
-  <input
-   type="text"
-   onChange={(e) => this.setState({ text: e.target.value })}
-  />
-</div>
-
-<div class="dropdown">
-  <button class="dropbtn">
-     Type:
-         <i class="fa fa-caret-down"></i>
-   </button>
-   <div class="dropdown-content">
-     <a href="">All</a>
-     <a href="">Public</a>
-     <a href="">Private</a>
-     <a href="">Sources</a>
-     <a href="">Forks</a>
-     <a href="">Archived</a>
-     <a href="">Mirrors</a>
-   </div>
- </div>
-
-
-  <div class="dropdown">
-   <button class="dropbtn">
-     Language
-     <i class="fa fa-caret-down"></i>
-   </button>
-  
-  
-    <div onSelect={e => this.setState({ lang: e.target.value })} class="dropdown-content">
-    
-
-     <a value='All' href="#">All</a>  
-     <a value='JavaScript' href="#">JavaScript</a>
-     <a value='Java' href="#">Java</a>
-     <a value='HTML' href="#">HTML</a>
-    
-   </div> 
- </div> 
-
- <div>
-   <button className="button">New</button>
- </div>
- </div>
- <div className="rep-row-3">{repositorie}</div>
- </div>  */}
